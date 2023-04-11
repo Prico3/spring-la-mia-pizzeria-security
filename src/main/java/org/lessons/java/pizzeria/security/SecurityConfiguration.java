c5d96230f258e56b240d2dd6aff03bcf09c3363f
Prico3 <pricoco.andrea@gmail.com>
2023-04-11 17:14:42 +0200

    Add roles and SecurityConfiguration

--- src/main/java/org/lessons/java/pizzeria/security/SecurityConfiguration.java
+++ src/main/java/org/lessons/java/pizzeria/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.lessons.java.pizzeria.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,8 +40,11 @@ public class SecurityConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers("/**")
-                .fullyAuthenticated()
+                .requestMatchers("/ingredients", "/ingredients/**").hasAuthority("Admin")
+                .requestMatchers("/pizzas/create", "/pizzas/edit/**", "pizzas/delete/**").hasAuthority("Admin")
+                .requestMatchers("/", "/pizzas", "/pizzas/**").hasAnyAuthority("User", "Admin")
+                .requestMatchers(HttpMethod.POST, "/pizzas/**").hasAuthority("Admin")
+                .requestMatchers("/**").permitAll()
                 .and().formLogin()
                 .and().logout()
                 .and().exceptionHandling();
